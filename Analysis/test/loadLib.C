ea51e4089a0fa5cd8cbce1892f3f7849ee66b59e
Ulascan Sarica <ulascan90@gmail.com>
2018-12-19 14:47:35 -0800

    Add dependence to cmstas/CORE

--- Analysis/test/loadLib.C
+++ Analysis/test/loadLib.C
@@ -1,7 +1,11 @@
 {
+  TString LIBCMS4CORE="CMS3_CORE.so";
+
   gSystem->Load("$CMSSW_BASE/src/CMSDataTools/AnalysisTree/test/loadLib.C");
 
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/STOP_1Lepton/Analysis/interface/");
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/STOP_1Lepton/Analysis/test/");
+  gSystem->AddIncludePath("-I$CMSSW_BASE/src/cmstas/CORE/");
+  gSystem->Load("$CMSSW_BASE/src/cmstas/CORE/" + LIBCMS4CORE);
   gSystem->Load("libSTOP_1LeptonAnalysis.so");
 }
